d04d75d01a53a9adda526a673e43c65db9fa986f
mahmoudor74 <mahmoud.orban47@gmail.com>
2023-05-30 11:21:32 +0300

    commit2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const filterItem = document.getElementById("filter");
 
 function addItem(e) {
   e.preventDefault();
@@ -13,12 +15,15 @@ function addItem(e) {
 
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(newItem));
-  
+
   const button = createButton("remove-item btn-link text-red");
 
   li.appendChild(button);
   itemList.appendChild(li);
-  newItem = "";
+
+  checkUI();
+
+  itemInput.value = "";
 }
 
 function createButton(classes) {
@@ -36,4 +41,55 @@ function createIcon(classes) {
   return icon;
 }
 
+function rempveItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+
+  checkUI();
+}
+
+function clearItems() {
+  if (confirm("Are you sure?")) {
+    while (itemList.firstChild) {
+      itemList.removeChild(itemList.firstChild);
+    }
+  }
+
+  checkUI();
+}
+
+function itemFilter(e) {
+  const text = e.target.value.toLowerCase();
+  const items = itemList.querySelectorAll("li");
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flix";
+    } else {
+        item.style.display = "none";
+    }
+  });
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    filterItem.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    filterItem.style.display = "block";
+  }
+}
+
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", rempveItem);
+clearBtn.addEventListener("click", clearItems);
+filterItem.addEventListener("input", itemFilter);
+
+checkUI();
